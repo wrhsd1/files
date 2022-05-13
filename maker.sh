a86d81e750c7ba7026f0129f3a7ecc969afd46d4
wrhsd1 <wangrenhuan@gmail.com>
2022-05-14 07:10:09 +0800

    Create maker.sh

---
+++ maker.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+export MACASARF='/workdir/trilium-trans-release/trilium-mac-x64/Trilium Notes.app/Contents/Resources/'
+export MACASARP='/workdir/trilium-trans-release/trilium-mac-x64/Trilium Notes.app/Contents/Resources/app.asar'
+export MACASARA='/workdir/trilium-trans-release/trilium-mac-x64/Trilium Notes.app/Contents/Resources/app/'
