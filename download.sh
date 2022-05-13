617e8bf08807fb8c5e5879bf4cb58b510e87a8b4
wrhsd1 <wangrenhuan@gmail.com>
2022-05-13 18:05:39 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -6,8 +6,8 @@ VERSION=`jq -r ".version" package.json`
 wget https://github.com/zadam/trilium/releases/download/v${VERSION}/trilium-linux-x64-${VERSION}.tar.xz
 tar -xf trilium-linux-x64-${VERSION}.tar.xz && mv trilium-linux-x64 /workdir/trilium-trans/
 cd /workdir/trilium-trans-release
-wget https://github.com/zadam/trilium/releases/download/v${VERSION}/trilium-linux-x64-${VERSION}.tar.xz
-wget https://github.com/zadam/trilium/releases/download/v${VERSION}/trilium-linux-x64-server-${VERSION}.tar.xz
-wget https://github.com/zadam/trilium/releases/download/v${VERSION}/trilium-mac-x64-${VERSION}.zip
-wget https://github.com/zadam/trilium/releases/download/v${VERSION}/trilium-windows-x64-${VERSION}.zip
+wget https://github.com/zadam/trilium/releases/download/v${VERSION}/trilium-linux-x64-${VERSION}.tar.xz  -O trilium-linux-x64.tar.xz
+wget https://github.com/zadam/trilium/releases/download/v${VERSION}/trilium-linux-x64-server-${VERSION}.tar.xz  -O trilium-linux-x64-server.tar.xz
+wget https://github.com/zadam/trilium/releases/download/v${VERSION}/trilium-mac-x64-${VERSION}.zip  -O trilium-mac-x64.zip
+wget https://github.com/zadam/trilium/releases/download/v${VERSION}/trilium-windows-x64-${VERSION}.zip  -O trilium-windows-x64.zip
 ls
