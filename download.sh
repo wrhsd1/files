a4f5ea3fb5cd2f504a481ffe7eb574dea5723874
wrhsd1 <wangrenhuan@gmail.com>
2022-05-13 15:12:30 +0800

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+VERSION=`jq -r ".version" package.json`
+
+git clone https://github.com/zadam/trilium.git /workdir/trilium-trans/trilium-src
+wget https://github.com/zadam/trilium/releases/download/v{VERSION}/trilium-linux-x64-{VERSION}.tar.xz
+tar -xf trilium-linux-x64-{VERSION}.tar.xz && mv trilium-linux-x64 /workdir/trilium-trans/
+cd /workdir/trilium-trans-release
+wget https://github.com/zadam/trilium/releases/download/v{VERSION}/trilium-linux-x64-{VERSION}.tar.xz
+wget https://github.com/zadam/trilium/releases/download/v{VERSION}/trilium-linux-x64-server-{VERSION}.tar.xz
+wget https://github.com/zadam/trilium/releases/download/v{VERSION}/trilium-mac-x64-{VERSION}.zip
+wget https://github.com/zadam/trilium/releases/download/v{VERSION}/trilium-windows-x64-{VERSION}.zip
+ls
