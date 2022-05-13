f79615077374bdc69fc69283bbb61c94ccd3ef28
wrhsd1 <wangrenhuan@gmail.com>
2022-05-13 16:17:19 +0800

    Create rename.sh

---
+++ rename.sh
@@ -0,0 +1,4 @@
+mv trilium-linux-x64-*.xz trilium-linux-x64-latest.tar.xz
+mv trilium-linux-x64-server-*.tar.xz trilium-linux-x64-server-latest.tar.xz
+mv trilium-mac-x64-*.zip trilium-mac-x64-latest.zip
+mv trilium-windows-x64-*.zip trilium-windows-x64-latest.zip
